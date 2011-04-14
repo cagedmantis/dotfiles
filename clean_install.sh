cfca941b87823e15d8f8f4a8ec3f9f2c4018d51a
cagedmantis <carlos.amedee@gmail.com>
2011-04-14 16:10:54 -0400

    starting a list of all of my essential software.

---
+++ clean_install.sh
@@ -0,0 +1,7 @@
+
+
+
+sudo add-apt-repository "deb http://archive.canonical.com/ maverick partner"
+sudo apt-get update   
+sudo apt-get install sun-java6-jre sun-java6-plugin
+sudo update-alternatives --config java
