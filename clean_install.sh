e8c94ea4e8562ea1284e8985bd8d68c7873060cc
Carlos Amedee <carlos.amedee@gmail.com>
2011-10-27 09:42:18 -0400

    Added some simple items to the clean install file.

--- clean_install.sh
+++ clean_install.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+# Ubuntu bash script to get everything you want installed
+# Work in progress!
 
+# Update system
+sudo apt-get update
 
+# Install Sun Java 6   
+sudo apt-get install sun-java6-jre sun-java6-plugin sun-java6-jdk
 
-sudo add-apt-repository "deb http://archive.canonical.com/ maverick partner"
-sudo apt-get update   
-sudo apt-get install sun-java6-jre sun-java6-plugin
+# Install Git
+sudo apt-get install git-core git-gui git-docInstalls git-core, git-gui, and git-doc
 sudo update-alternatives --config java
+
+# email client
+sudo apt-get install alpine
