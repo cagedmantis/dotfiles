55765edfb0246939f50e9541956b1bd187460bae
Carlos Amedee <carlos@Carloss-iMac.local>
2015-03-26 16:36:45 -0400

    Updating osx apps.

--- clean_install_osx.sh
+++ clean_install_osx.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 # Install OS X essentials
 
-#osx
+#brew
 brew install aspell --lang=en
-
+brew install alpine
 brew install mit-scheme
-
+brew install git
 brew install whatmask
+brew install ssh-copy-id
+brew install cowsay
+brew install wget
+brew install tmux
+brew install sysdig
+brew install htop-osx
+brew install go
+brew install fortune
+brew install cmake
+brew install autoconf
+brew install automake
+brew install git-flow
+brew install git-extras
+brew install ctags
+brew install mtr
+brew install readline
+
+#gem
+sudo gem install lolcat
+
