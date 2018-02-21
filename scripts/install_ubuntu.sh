52b9428dddb6492c7633740e91ac19caea0ea058
Carlos Amedee <carlos.amedee@gmail.com>
2018-02-21 04:32:10 -0500

    Add docker and kvm

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -2,35 +2,39 @@
 
 sudo apt-get update && \
     sudo apt-get install \
-	 alpine \
-	 aspell-en \
-	 aspell-es \
-	 build-essential \
-	 bzip2 \
-	 ca-certificates \
-	 clang \
-	 curl \
-	 emacs \
-	 fping \
-	 git \
-	 gnupg \
-	 gnupg-agent \
-	 gnupg2 \
-	 htop \
-	 jq \
-	 netcat \
-	 netmask \
-	 python-pip \
-	 python-setuptools \
-	 silversearcher-ag \
-	 tcpdump \
-	 tmux \
-	 tree \
-	 unzip \
-	 vim \
-	 vlan \
-	 xclip \
-	 zip
+		 alpine \
+		 apt-transport-https \
+		 aspell-en \
+		 aspell-es \
+		 build-essential \
+		 bzip2 \
+		 ca-certificates \
+		 ca-certificates \
+		 clang \
+		 curl \
+		 curl \
+		 emacs \
+		 fping \
+		 git \
+		 gnupg \
+		 gnupg-agent \
+		 gnupg2 \
+		 htop \
+		 jq \
+		 netcat \
+		 netmask \
+		 python-pip \
+		 python-setuptools \
+		 silversearcher-ag \
+		 software-properties-common \
+		 tcpdump \
+		 tmux \
+		 tree \
+		 unzip \
+		 vim \
+		 vlan \
+		 xclip \
+		 zip
 
 # because latex
 sudo apt-get install \
@@ -44,3 +48,17 @@ sudo apt-get install \
 	 texlive-latex-extra \
 	 texlive-latex-recommended \
 	 texlive-pictures
+
+# Docker
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+	 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update && \
+	sudo apt-get install docker-ce
+
+
+sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-manager
